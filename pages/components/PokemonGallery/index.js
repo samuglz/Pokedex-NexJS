e19e2d145ee8e52b15e727f9e76a442202f59beb
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-02-07 23:14:40 +0000

    Add InfiniteScroll

--- pages/components/PokemonGallery/index.js
+++ pages/components/PokemonGallery/index.js
@@ -1,33 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Card from "../Card";
 
 export default function index() {
   const [pokemons, setPokemons] = useState([]);
-  const [limitPerFetch, setLimitPerFetch] = useState(0);
+  const [offsetFetch, setOffsetFetch] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const getRealIndexPokedex = (arrayIndex) => {
-    return arrayIndex + limitPerFetch + 1;
+    return arrayIndex + offsetFetch + 1;
   };
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`)
-      .then((res) => res.json())
-      .then((data) => {
-        const { results: pokemons } = data;
-        setPokemons(
-          pokemons.map((pokemon, index) => {
-            return { id: getRealIndexPokedex(index), name: pokemon.name };
-          })
-        );
-      })
-      .catch((err) => console.error(err));
+    document.addEventListener("scroll", () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        setLoading(true);
+        setOffsetFetch((offsetFetch) => offsetFetch + 21);
+      }
+    });
   }, []);
 
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offsetFetch}&limit=21`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { results } = data;
+        const newPokemons = results.map((pokemon, index) => {
+          return { id: getRealIndexPokedex(index), name: pokemon.name };
+        });
+        const totalPokemon = [...pokemons, ...newPokemons];
+        setPokemons([...totalPokemon]);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, [offsetFetch]);
+
   return (
-    <div className="grid gap-6 grid-flow-row lg:grid-cols-3 md:grid-cols-2">
-      {pokemons.map((pokemon) => {
-        return <Card key={pokemon.id} pokemon={pokemon} />;
-      })}
+    <div className="flex flex-col justify-center items-center">
+      <div className="grid gap-6 grid-flow-row lg:grid-cols-3 md:grid-cols-2">
+        {pokemons.map((pokemon) => {
+          return <Card key={pokemon.id} pokemon={pokemon} />;
+        })}
+      </div>
+      {loading && (
+        <div className="w-10 h-10 border-b-4 border-l-4 border-t-4 animate-spin border-gray-500 border-solid rounded-full my-20"></div>
+      )}
     </div>
   );
 }
