1a7fa34c18e9bdcfced30fa33fea4ee4770beaee
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-02-15 17:09:40 +0000

    Now the error message to be displayed can be received as prop

--- components/Error/index.js
+++ components/Error/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Error() {
+export default function Error({
+  msgError = "Oops, something went wrong, try again later.",
+}) {
   return (
     <div className="flex justify-center items-center flex-col">
       <div className="bg-red-300 p-4 font-bold text-red-700 text-xl md:text-3xl rounded shadow-md flex flex-col justify-center items-center">
         <span>❌</span>
-        <p className="text-center">
-          Oops, something went wrong, try again later.
-        </p>
+        <p className="text-center py-2">{msgError} </p>
       </div>
     </div>
   );
